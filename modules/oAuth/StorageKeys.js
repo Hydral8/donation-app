4cba6781f7c1f9008e6d26e59c5e0b6e0900fbfa
Sung Jae Bae <sbae703@gmail.com>
2020-02-23 12:53:45 -0800

    test

--- modules/oAuth/StorageKeys.js
+++
@@ -1,4 +0,0 @@
-const googleStorageKey =
-  "@GiftCardDonationApp:AIzaSyBlQ_pC8Gwmw45hFWj9D8UtMbSEdBFP1VY";
-
-export { googleStorageKey };
--- modules/oAuth/oAuthConfigs.js
+++
@@ -1,8 +0,0 @@
-const googleOAuthConfig = {
-  issuer: "https://accounts.google.com",
-  scopes: ["profile", "email"],
-  clientId:
-    "423009903286-cp54kq0m3ellnovk2qs8p66acj34nf3j.apps.googleusercontent.com"
-};
-
-export { googleOAuthConfig };
